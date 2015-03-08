2fbd9d35dc5bf5308ac87861e316e98914168b25
raul <raul@raul-VAIO>
2015-03-08 22:04:41 +0100

    me siento a empezar la practica

--- src/main/java/es/art83/persistence/models/entities/HelloJPA.java
+++ src/main/java/es/art83/persistence/models/entities/HelloJPA.java
@@ -1,44 +1,76 @@
 package es.art83.persistence.models.entities;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+
+import javax.security.auth.login.Configuration;
+
 import org.eclipse.persistence.config.PersistenceUnitProperties;
+import org.eclipse.persistence.sessions.factories.SessionFactory;
 
 public class HelloJPA {
     public static void main(String[] args) {
         //Se borrar y se crean las tablas
         Map<String, String> properties = new HashMap<>();
-        properties.put(PersistenceUnitProperties.DDL_GENERATION,
-                PersistenceUnitProperties.DROP_AND_CREATE);
-        EntityManager em = Persistence.createEntityManagerFactory("BBDD", properties)
-                .createEntityManager();
+//        properties. put(PersistenceUnitProperties.DDL_GENERATION,
+//                PersistenceUnitProperties.DROP_AND_CREATE);
+//        EntityManager em = Persistence.createEntityManagerFactory("BBDD", properties)
+//                .createEntityManager();
         
-        User1 u1 = new User1(1, "Sou u1");
-        // Create
+       
+        EntityManager em = Persistence.createEntityManagerFactory("BBDD")
+           .createEntityManager();
+        
+        Tema t1 = new Tema("tema1", "cual es ...");
+        Tema t2 = new Tema("tema2", "cual es ...");
+        Tema t3 = new Tema("tema2", "cual es ...");
+        
         em.getTransaction().begin();
-        em.persist(u1);
-        em.persist(new User1(2, "Soy dos"));
-        em.persist(new User1(3, "Soy tres"));
+        em.persist(t1);
+        em.persist(t2);
+        em.persist(t3);
+        em.flush();
+        t1.setPregunta("pregunta actualizada");
+        em.merge(t1);
+       
+        Voto v1 = new Voto("1", 5, "bachiller",t1);
+        Voto v2 = new Voto("34", 5, "bachiller",t2);
+        Voto v3 = new Voto("1", 5, "bachiller",t1);
+       em.persist(v1);
+       em.persist(v1);
+       em.persist(v2);
+       //em.persist(v3);
+       em.flush();
         em.getTransaction().commit();
-        // Read
-        System.out.println(em.find(User1.class, 1));
-        // Update
-        em.getTransaction().begin();
-        em.merge(new User1(2, "dos cambiado con update"));
-        em.getTransaction().commit();
-        // Update2
-        em.getTransaction().begin();
-        u1.setName("uno, cambiado con set");
-        em.getTransaction().commit();
-        // Delete
-        User1 u3 = em.find(User1.class, 3);
-        em.getTransaction().begin();
-        em.remove(u3);
-        em.getTransaction().commit();
+        
+//        User1 u1 = new User1(1, "Sou u1");
+//        // Create
+//        em.getTransaction().begin();
+//        em.persist(u1);
+//        em.persist(new User1(2, "Soy dos"));
+//        em.persist(new User1(3, "Soy tres"));
+//        em.getTransaction().commit();
+//        // Read
+//        System.out.println(em.find(User1.class, 1));
+//        // Update
+//        em.getTransaction().begin();
+//        em.merge(new User1(2, "dos cambiado con update"));
+//        em.getTransaction().commit();
+//        // Update2
+//        em.getTransaction().begin();
+//        u1.setName("uno, cambiado con set");
+//        em.getTransaction().commit();
+//        // Delete
+//        User1 u3 = em.find(User1.class, 3);
+//        em.getTransaction().begin();
+//        em.remove(u3);
+//        em.getTransaction().commit();
 
     }
 }
