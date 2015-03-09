7e178fabd129b84dff72fc72cbd932e3d0ec8d65
raul <raul@raul-VAIO>
2015-03-09 20:11:24 +0100

    Añadidos generic dao, y VotoDao, TemaDao

---
+++ src/main/java/es/miw/jeeecp/models/daos/VotoDao.java
@@ -0,0 +1,9 @@
+package es.miw.jeeecp.models.daos;
+
+import java.util.List;
+
+import es.miw.jeeecp.models.entities.VotoEntity;
+
+public interface VotoDao extends GenericDao<VotoEntity, Integer> {
+ 
+}
